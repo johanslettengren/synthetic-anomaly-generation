feb7480db9d9ee9f850cf5bf76a2d26d341ade3c
johansle <johansle@kth.se>
2025-08-04 15:47:32 +0200

    update

--- static-modelling/static_model.py
+++ static-modelling/static_model.py
@@ -2,14 +2,21 @@ import torch
 import torch.nn as nn
 import matplotlib.pyplot as plt
 
+import numpy as np
+import networkx as nx
+
 import itertools
 
 
 from numpy import pi
 from scipy.constants import g
 
+import torch.nn.functional as F
+
 softplus = nn.Softplus()
 
+def abs_(x, epsilon=1e-4):
+    return torch.abs(x)
 
 class Net(nn.Module):
 
@@ -17,7 +24,7 @@ class Net(nn.Module):
         super().__init__()
         
         self.positive = positive
-        self.activation = {'relu' : nn.ReLU(), 'tanh' : nn.Tanh(), 'sigmoid' : nn.Sigmoid()}[activation] 
+        self.activation = {'relu' : nn.ReLU(), 'tanh' : nn.Tanh(), 'sigmoid' : nn.Sigmoid(), 'softplus' : softplus}[activation] 
         
         
         layer_sizes = [dim_in] + layer_sizes
@@ -26,7 +33,7 @@ class Net(nn.Module):
         for i in range(1, len(layer_sizes)):
             layer = nn.Linear(layer_sizes[i - 1], layer_sizes[i])
             nn.init.xavier_normal_(layer.weight)
-            nn.init.constant_(layer.bias, 0.01)
+            nn.init.constant_(layer.bias, 0.1)
             self.linears.append(layer)
             
         
@@ -40,6 +47,126 @@ class Net(nn.Module):
             x = softplus(x)
         return x
     
+
+class Model():
+    def __init__(self, model_params, net_params):  
+        
+        required_keys = ['A0', 'inv', 'M', 'B', 'A_max', 'S', 'D', 'L', 'd', 'Cd', 'C', 'rho', 'n_samples']
+
+        # Assert all required keys are present
+        missing = [key for key in required_keys if key not in model_params]
+        if missing:
+            raise KeyError(f"Missing required model parameters: {missing}")
+
+        # Assign attributes
+        for key in required_keys:
+            setattr(self, key, model_params[key])
+                    
+        #self.net = GNN(self.A0, self.D, self.B, self.S, **net_params)
+        self.net = Net(self.A0.shape[1], **net_params, positive=True)
+        
+        #params = itertools.chain(self.Hnet.parameters(), self.qnet.parameters())
+        
+                
+        optimizer = torch.optim.Adam
+        self.optimizer = optimizer(self.net.parameters(), lr=0.001)
+        self.mse = lambda x : (x**2).mean()
+        
+        self.bestloss = 1e12
+                
+        self.mse = lambda x : (x**2).mean()
+
+    def hL(self, q):
+        return torch.sign(q) * 10.667 * self.C**(-1.852) * self.d**(-4.871) * self.L * abs_(q)**(1.852)
+    
+    def d_leak(self, A, H):
+        
+        d = torch.sign(qself.Cd * A * torch.sqrt(2 * g * abs_(H))
+        return d. 
+    
+    
+    def mv(self, M, v):
+        v_ = v.reshape(-1, v.shape[-1])
+        return (M @ v_.T).T.reshape(v.shape[0], v.shape[1], -1)
+    
+
+    
+    def loss_(self, a):
+        
+        H = self.net(a)
+            
+        hL = (self.B @ self.S)[None,:None] - self.mv(self.A0.T, H).squeeze(-1)
+        
+
+        q = (torch.sign(hL) * (abs_(hL) * self.C[None,:]**(1.852) * self.d[None,:]**(4.871) / 10.667 / self.L[None,:])**(1 / 1.852))
+        loss = self.mse(self.mv(self.A0, q).squeeze(-1) - self.D[None,:] - (self.M @ self.d_leak(a, (self.M.T @ H.T).T).T).T)
+        
+        return loss 
+    
+    def loss(self, a):
+        
+        q = self.net(a)
+        
+        hL = self.hL(q)
+        
+                
+        H = (self.inv @ ((self.B @ self.S)[None,:] - hL).T).T
+        
+        loss = self.mse(self.mv(self.A0, q).squeeze(-1) - self.D[None,:] - self.d_leak(a,H))
+        
+        return loss 
+
+            
+    def train(self, iterations, print_interval=100):
+        self.steps = []
+        self.net.train(True)
+        print(f"{'step':<10} {'loss':<10} {'e1':<10}  {'e2'}")
+        
+        for iter in range(iterations):
+            
+            
+            A = torch.linspace(0, self.A_max, self.n_samples).reshape(-1, 1).requires_grad_()  
+            
+            A = torch.cat((A, torch.zeros_like(A)), dim=-1)                                    
+                                    
+            self.optimizer.zero_grad()
+            loss = self.loss(A)
+            loss.backward()
+            torch.nn.utils.clip_grad_norm_(self.net.parameters(), max_norm=1.0)
+            self.optimizer.step()
+            
+            vloss = loss.item()
+            
+            announce = ''
+            if iter % print_interval == print_interval - 1:   
+                             
+                if vloss < self.bestloss:
+                    torch.save(self.net.state_dict(), "best_model.pth")    
+                    self.bestloss = loss
+                    
+                    announce = 'New Best!'
+                # {f'{e1:.2e}':<10} {f'{e2:.2e}':<15}                
+                fstring = f"{iter + 1:<10} {f'{vloss:.2e}':<10} {announce}"
+                print(fstring)
+                
+        
+    def eval(self):
+        print('Best loss:', self.bestloss)
+        self.net.load_state_dict(torch.load("best_model.pth", weights_only=True))
+        self.net.eval()
+
+
+def loss_(self, a):
+                    
+    H, q = self.net(a)
+
+        
+            
+    e1 = self.mse(self.mv(self.A0, q) - self.D - self.d_leak(a, H)) 
+    e2 = self.mse(self.B @ self.S - self.mv(self.A0.T, H) - self.hL(q))
+    
+    return e1, e2
+
 class GNN(nn.Module):
 
     def __init__(self, A0, D, B, S, depth, layer_sizes, activation):
@@ -101,101 +228,3 @@ class GNN(nn.Module):
             
 
         return H.squeeze(-1), q.squeeze(-1)
-    
-
-class Model():
-    def __init__(self, model_params, net_params):  
-        
-        required_keys = ['A0', 'B', 'A_max', 'S', 'D', 'L', 'd', 'Cd', 'C', 'rho', 'n_samples']
-
-        # Assert all required keys are present
-        missing = [key for key in required_keys if key not in model_params]
-        if missing:
-            raise KeyError(f"Missing required model parameters: {missing}")
-
-        # Assign attributes
-        for key in required_keys:
-            setattr(self, key, model_params[key])
-                    
-        #self.net = GNN(self.A0, self.D, self.B, self.S, **net_params)
-        self.net = Net(self.A0.shape[0], **net_params, positive=False)
-        
-        #params = itertools.chain(self.Hnet.parameters(), self.qnet.parameters())
-        
-                
-        optimizer = torch.optim.Adam
-        self.optimizer = optimizer(self.net.parameters(), lr=0.001)
-        self.mse = lambda x : (x**2).mean()
-        
-        self.bestloss = 1e12
-                
-        self.mse = lambda x : (x**2).mean()
-
-    def hL(self, q):
-        return torch.sign(q) * 10.667 * self.C**(-1.852) * self.d**(-4.871) * self.L * torch.abs(q)**(1.852)
-    
-    def d_leak(self, A, H):
-        
-        d = self.Cd * A * torch.sqrt(2 * g * torch.abs(H))
-        #print(self.Cd)
-        #print(A.shape)
-        #print(H.shape)
-        #print(d.shape)
-        return d
-    
-    def mv(self, M, v):
-        v_ = v.reshape(-1, v.shape[-1])
-        return (M @ v_.T).T.reshape(v.shape[0], v.shape[1], -1)
-
-    def loss_(self, a):
-                
-        H, q = self.net(a)
-    
-            
-                
-        e1 = self.mse(self.mv(self.A0, q) - self.D - self.d_leak(a, H)) 
-        e2 = self.mse(self.B @ self.S - self.mv(self.A0.T, H) - self.hL(q))
-        
-        return e1, e2
-    
-    def loss(self, A):
-        
-        H = self.net(A)
-        hL = (self.B @ self.S)[None,:None] - self.mv(self.A0.T, H).squeeze(-1)
-
-        q = (torch.sign(hL) * (torch.abs(hL) * self.C[None,:]**(1.852) * self.d[None,:]**(4.871) / 10.667 / self.L[None,:])**(1 / 1.852))
-        loss = self.mse(self.mv(self.A0, q).squeeze(-1) - self.D[None,:] - self.d_leak(A, H))
-        return loss 
-
-            
-    def train(self, iterations, print_interval=100):
-        self.steps = []
-        self.net.train(True)
-        print(f"{'step':<10} {'loss':<10} {'e1':<10}  {'e2'}")
-        
-        for iter in range(iterations):
-            A = torch.linspace(0, self.A_max, self.n_samples).reshape(-1, 1).requires_grad_()   
-                                    
-                                    
-            self.optimizer.zero_grad()
-            loss = self.loss(A)
-            loss.backward()
-            vloss = loss.item()
-
-            self.optimizer.step()
-            
-            announce = ''
-            if iter % print_interval == print_interval - 1:           
-                if vloss < self.bestloss:
-                    torch.save(self.net.state_dict(), "best_model.pth")    
-                    self.bestloss = loss
-                    
-                    announce = 'New Best!'
-                # {f'{e1:.2e}':<10} {f'{e2:.2e}':<15}                
-                fstring = f"{iter + 1:<10} {f'{vloss:.2e}':<10} {announce}"
-                print(fstring)
-                
-        print('Best loss:', self.bestloss)
-        
-        self.net.load_state_dict(torch.load("best_model.pth", weights_only=True))
-        self.net.eval()
